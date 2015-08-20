8a1d71627d27a3b66484518307cf590610280ad9
Nikolay Voronkov <info@nikolayvoronkov.com>
2015-08-20 12:30:22 +0000

    I think i fixed the no source payment error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819145536) do
+ActiveRecord::Schema.define(version: 20150820121807) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
